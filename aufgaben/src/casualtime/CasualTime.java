eb882b0a498a38014f0db5ee6143a42343a6e3a2
jasxsin <jasjot.singh@students.fhnw.ch>
2026-08-19 12:14:28 +0200

    Aufgabe fertig without refactoring

--- aufgaben/src/casualtime/CasualTime.java
+++ aufgaben/src/casualtime/CasualTime.java
@@ -14,7 +14,7 @@ public class CasualTime {
 
         while (!input.equals("quit")) {
             var casual = convertToCasual(input);
-            System.out.println("It's " + casual + ".");
+            System.out.println(casual + ".");
 
             System.out.print("> ");
             input = scanner.nextLine();
@@ -23,27 +23,60 @@ public class CasualTime {
 
     public static String convertToCasual(String formal) {
 
-        //String eingabe convertieren zu Zahlen
+
+        if (formal == null || formal.isEmpty()) {return "Input empty";}
+        if (!formal.matches("[0-9:]+")) {return "Invalid Input try again!";}
+
+        //String input converting to numbers
         String[] parts = formal.split(":");
+        if (parts.length != 2 || parts[0].isEmpty() || parts[1].isEmpty()) {
+            return "Please include hours and minutes";
+        }
         int hour = Integer.parseInt(parts[0]);
         int minute = Integer.parseInt(parts[1]);
 
-        // 0-12 nicht 0-24
+        //Check if hours in 0-23 range and minutes is in 0-59 range
+        if (hour < 0 || hour > 23) { return ("Invalid input. The hours are from 0 to 23!");}
+        if (minute < 0 || minute > 59) { return ("Invalid input. The minutes are from 0-59!");}
+
+        // 0-12 not 0-24
         int hour12 = hour % 12;
         if(hour12 == 0){
             hour12 = 12;
         }
 
-        //Zeiten ohne minute
+        int nextHour = hour12 + 1;
+        if(nextHour == 13){
+            nextHour = 1;
+        }
+
+        //Time wtihout minutes
         if(minute == 0){
             return hour12 + " o'clock";
         }
 
+        //30 minutes
+        if(minute == 30){
+            return "half past " + hour12;
+        }
 
-        // TODO: Zeitangabe in Parameter 'formal' in Stunde und Minute
-        //  aufteilen und anschliessend in Umgangssprache umwandeln.
-        //  Tipp: Methoden 'split' von String und 'Integer.parseInt'.
+        //quarter past&to
+        if(minute == 45){
+            return "quarter to " + nextHour;
+        } else if (minute == 15){
+            return "quarter past " + hour12;
+        }
 
-        return "TODO";
+        // minutes past&to
+        if(minute < 30 && minute > 1){
+            return minute + " past " + hour12;
+        } else if (minute > 30 && minute < 59) {
+            return (60-minute) + " minutes to " + nextHour;
+            // Singular 1 minute to or past
+        } else if (minute == 1){
+            return minute + " minute past " + hour12;
+        } else {
+            return (60-minute) + " minute to " + nextHour;
+        }
     }
 }
--- aufgaben/test/casualtime/CasualTimeTest.java
+++ aufgaben/test/casualtime/CasualTimeTest.java
@@ -3,7 +3,6 @@ package casualtime;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CasualTimeTest {
 
@@ -37,37 +36,52 @@ class CasualTimeTest {
 
     @Test
     void past(){
-        assertEquals("20 minute past 4", CasualTime.convertToCasual("04:20"));
-        assertEquals("20 minute past 4", CasualTime.convertToCasual("16:20"));
+        assertEquals("20 past 4", CasualTime.convertToCasual("04:20"));
+        assertEquals("20 past 4", CasualTime.convertToCasual("16:20"));
+        assertEquals("25 past 9", CasualTime.convertToCasual("09:25"));
+        assertEquals("25 past 9", CasualTime.convertToCasual("21:25"));
+        assertEquals("28 past 11", CasualTime.convertToCasual("23:28"));
+    }
+
+    @Test
+    void minutesTo(){
+        assertEquals("20 minutes to 10", CasualTime.convertToCasual("09:40"));
+        assertEquals("20 minutes to 10", CasualTime.convertToCasual("21:40"));
+        assertEquals("10 minutes to 10", CasualTime.convertToCasual("09:50"));
+        assertEquals("10 minutes to 10", CasualTime.convertToCasual("21:50"));
+        assertEquals("5 minutes to 10", CasualTime.convertToCasual("09:55"));
+        assertEquals("18 minutes to 10", CasualTime.convertToCasual("09:42"));
+        assertEquals("11 minutes to 6", CasualTime.convertToCasual("17:49"));
+    }
+
+    @Test
+    void quarter(){
+        assertEquals("quarter to 10", CasualTime.convertToCasual("09:45"));
+        assertEquals("quarter to 10", CasualTime.convertToCasual("21:45"));
+        assertEquals("quarter to 1", CasualTime.convertToCasual("12:45"));
+        assertEquals("quarter to 3", CasualTime.convertToCasual("14:45"));
         assertEquals("quarter past 1", CasualTime.convertToCasual("01:15"));
         assertEquals("quarter past 1", CasualTime.convertToCasual("13:15"));
-        assertEquals("25 minute past 9", CasualTime.convertToCasual("09:25"));
+    }
+
+    @Test
+    void singularMinute(){
+        assertEquals("1 minute to 1", CasualTime.convertToCasual("00:59"));
+        assertEquals("1 minute to 1", CasualTime.convertToCasual("12:59"));
         assertEquals("1 minute past 12", CasualTime.convertToCasual("00:01"));
         assertEquals("1 minute past 1", CasualTime.convertToCasual("13:01"));
     }
 
     @Test
-    void minutesTo(){
-        assertEquals("25 minutes to 10", CasualTime.convertToCasual("09:35"));
-        assertEquals("20 minutes to 10", CasualTime.convertToCasual("09:40"));
-        assertEquals("20 minutes to 10", CasualTime.convertToCasual("21:40"));
-        assertEquals("quarter to 10", CasualTime.convertToCasual("09:45"));
-        assertEquals("quarter to 10", CasualTime.convertToCasual("21:45"));
-        assertEquals("10 minutes to 10", CasualTime.convertToCasual("09:50"));
-        assertEquals("10 minutes to 10", CasualTime.convertToCasual("21:50"));
-        assertEquals("5 minutes to 10", CasualTime.convertToCasual("09:55"));
-        assertEquals("18 minutes to 10", CasualTime.convertToCasual("09:42"));
-        assertEquals("1 minute to 1", CasualTime.convertToCasual("00:59"));
-
-    }
-
-    @Test
     void invalidInputs() {
-        assertThrows(IllegalArgumentException.class, () -> CasualTime.convertToCasual("24:00"));
-        assertThrows(IllegalArgumentException.class, () -> CasualTime.convertToCasual("12:60"));
-        assertThrows(IllegalArgumentException.class, () -> CasualTime.convertToCasual("ab:cd"));
-        assertThrows(IllegalArgumentException.class, () -> CasualTime.convertToCasual(""));
-        assertThrows(IllegalArgumentException.class, () -> CasualTime.convertToCasual("123"));
+        assertEquals("Invalid input. The hours are from 0 to 23!", CasualTime.convertToCasual("24:00"));
+        assertEquals("Invalid input. The minutes are from 0-59!", CasualTime.convertToCasual("12:60"));
+        assertEquals("Invalid Input try again!", CasualTime.convertToCasual("ab:cd"));
+        assertEquals("Invalid Input try again!", CasualTime.convertToCasual("df"));
+        assertEquals("Input empty", CasualTime.convertToCasual(""));
+        assertEquals("Please include hours and minutes", CasualTime.convertToCasual("123"));
+        assertEquals("Please include hours and minutes", CasualTime.convertToCasual("123:"));
+        assertEquals("Please include hours and minutes", CasualTime.convertToCasual(":234"));
     }
 
 }
