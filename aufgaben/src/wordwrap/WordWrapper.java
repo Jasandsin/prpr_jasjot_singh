a8f667ae7e77a3e0328c83dccddb1f610f6b5fb8
jasxsin <jasjot.singh@students.fhnw.ch>
2026-08-23 23:15:52 +0200

    word wrap implementiert und Tests grün.

--- aufgaben/src/wordwrap/WordWrapper.java
+++ aufgaben/src/wordwrap/WordWrapper.java
@@ -1,5 +1,7 @@
 package wordwrap;
 
+import java.util.Scanner;
+
 public class WordWrapper {
 
     private final int maxLinesLength;
@@ -9,6 +11,28 @@ public class WordWrapper {
     }
 
     public String wrap (String text){
-        return text;
+
+        Scanner scanner = new Scanner(text);
+        String result = "";
+        int lineLength = 0;
+        int emptySpace = 1;
+
+        while(scanner.hasNext()){
+            String word = scanner.next();
+
+            if (lineLength == 0){
+                result = result + word;
+                lineLength = word.length();
+
+            } else if (lineLength + emptySpace + word.length() <= maxLinesLength){
+                result = result + " " + word;
+                lineLength = lineLength + emptySpace + word.length();
+
+            } else {
+                result = result + "\n" + word;
+                lineLength = word.length();
+            }
+        }
+        return result;
     }
 }
