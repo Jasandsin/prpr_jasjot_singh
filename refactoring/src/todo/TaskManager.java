bcafa76e6696c0f4c74e2c8850e8501d305c6d60
jasxsin <jasjot.singh@students.fhnw.ch>
2026-08-24 10:27:24 +0200

    • R: convert static method printTaskList to instance method

--- refactoring/src/todo/TaskManager.java
+++ refactoring/src/todo/TaskManager.java
@@ -6,7 +6,7 @@ public class TaskManager {
     public TaskManager() {
     }
 
-    static String printTaskList(List<String> tasks) {
+    String printTaskList(List<String> tasks) {
         StringBuilder taskList = new StringBuilder();
         for (int i = 0; i < tasks.size(); i++) {
             taskList.append(i + 1).append(". ").append(tasks.get(i)).append("\n");
--- refactoring/src/todo/TodoApp.java
+++ refactoring/src/todo/TodoApp.java
@@ -46,14 +46,14 @@ public class TodoApp {
         textField.setTextColor(new Color(0, 0, 0));
 
         var textArea = new TextArea(10, 20 + textField.getHeight(), 380, 200);
-        textArea.setText(TaskManager.printTaskList(tasks));
+        textArea.setText(new TaskManager().printTaskList(tasks));
 
         var addButton = new Button("Add", WIDTH - 120, HEIGHT - 40, 50, 30) {
             public void onLeftClick(double _x, double _y) {
                 if (!currentTask.isEmpty()) {
                     tasks.add(currentTask);
                 }
-                textArea.setText(TaskManager.printTaskList(tasks));
+                textArea.setText(new TaskManager().printTaskList(tasks));
                 currentTask = "";
                 textField.setText("");
             }
