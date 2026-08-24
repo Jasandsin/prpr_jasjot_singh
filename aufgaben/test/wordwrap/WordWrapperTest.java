a5106d76bc3322cc8c0db4bbc443e04a96dc52e0
jasxsin <jasjot.singh@students.fhnw.ch>
2026-08-24 08:45:13 +0200

    Aufgabe Word wrap und reflexion notizen fertig

--- aufgaben/test/wordwrap/WordWrapperTest.java
+++ aufgaben/test/wordwrap/WordWrapperTest.java
@@ -31,4 +31,40 @@ class WordWrapperTest {
             "Umbruch"));
 
     }
+
+
+    @Test
+    void textWithoutChanges(){
+        WordWrapper wrapper = new WordWrapper(20);
+
+        assertEquals("Wie gehts dir?", wrapper.wrap("Wie gehts dir?"));
+    }
+
+    @Test
+    void textWithChanges(){
+        WordWrapper wrapper = new WordWrapper(10);
+
+        assertEquals("Wie gehts\n" + "dir?", wrapper.wrap("Wie gehts dir?"));
+    }
+
+    @Test
+    void textExactMaxLineLength(){
+        WordWrapper wrapper = new WordWrapper(7);
+        assertEquals("Exactly", wrapper.wrap("Exactly"));
+
+    }
+
+    @Test
+    void removeSpaces(){
+        WordWrapper wrapper = new WordWrapper(30);
+        assertEquals("Was meinst du damit?", wrapper.wrap("Was    meinst     du      damit?      "));
+
+    }
+
+    @Test
+    void removeBreaks() {
+        WordWrapper wrapper = new WordWrapper(20);
+
+        assertEquals("Was machst du hier?", wrapper.wrap("Was machst du \nhier?"));
+    }
 }
