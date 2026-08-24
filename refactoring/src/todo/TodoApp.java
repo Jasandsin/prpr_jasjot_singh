478ecdceb68230a9ec63a92adb6d73cb373151bc
jasxsin <jasjot.singh@students.fhnw.ch>
2026-08-24 10:49:07 +0200

    R: extract instance method for clear

--- refactoring/src/todo/TodoApp.java
+++ refactoring/src/todo/TodoApp.java
@@ -57,7 +57,7 @@ public class TodoApp {
         };
         var clearButton = new Button("Clear", WIDTH - 60, HEIGHT - 40, 50, 30) {
             public void onLeftClick(double _x, double _y) {
-                tasks.clear();
+                clear();
                 currentTask = "";
                 textField.setTextColor(new Color(100, 100, 100));
                 textField.setText("Add new Task");
@@ -68,6 +68,10 @@ public class TodoApp {
         gui.addComponents(textField, textArea, addButton, clearButton);
         gui.open();
         gui.runUntilClosed();
+    }
+
+    private static void clear() {
+        tasks.clear();
     }
 
 
